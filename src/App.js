f83f1129593e3a7c00905feafc437039bf8c3794
RohitWeb21 <Rohitweb21@gmail.com>
2021-10-08 00:49:59 +0530

    bs-group-contact-form

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ContactForm from "./components/ContactForm"
+const App = () => {
+  return (
+    <React.Fragment>
+      <ContactForm/>
+    </React.Fragment>
+  );
+};
+export default App;
